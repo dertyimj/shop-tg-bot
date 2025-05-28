067cf57c6fd52d2559b4fa95d1f3f6993845a94e
dertyimj <160622210+dertyimj@users.noreply.github.com>
2025-05-28 22:37:03 +0300

    tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,20 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        mainbg: "#24252d"
+        mainbg: "#222034",
+        cardbg: "#292744",
+        accent: "#8f7cff",      // красивый фиолетовый вайб
+        accent2: "#00ffe7",     // бирюзовый для кнопок
+        btnbg: "#3e3266",
+        pink: "#ff6dc2",
+        vibebg: "#232850",
+        green: "#12c988",
+      },
+      fontFamily: {
+        inter: ["Inter", "Arial", "sans-serif"]
+      },
+      boxShadow: {
+        'card': '0 4px 20px 0 rgba(80,60,180,0.15)',
       }
     }
   },
