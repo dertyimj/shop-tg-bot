c9c8ffe380642600868d396a699ad959a16f2703
dertyimj <160622210+dertyimj@users.noreply.github.com>
2025-05-29 00:25:15 +0300

    Create SupportChat.jsx

---
+++ src/components/SupportChat.jsx
@@ -0,0 +1,8 @@
+// src/components/SupportChat.jsx
+export default function SupportChat() {
+  return (
+    <div style={{padding: 24, color: "#fff"}}>
+      Support Chat: здесь появится поддержка!
+    </div>
+  );
+}
